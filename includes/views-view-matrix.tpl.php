d56b7da08e460d6e6a4f05040204f1d7822bd785
Matthew Radcliffe <mradcliffe@kosada.com>
2012-09-13 16:28:39 -0400

    Initial working code commit. The basic plugin works, but needs work for additional views features.

---
+++ includes/views-view-matrix.tpl.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * @file
+ *   Views View Matrix Template
+ *
+ * Variables:
+ *   - $header
+ *   - $rows
+ *   - $row_classes
+ */
+?>
+<table class="views-matrix">
+  <?php if (!empty($title)) : ?>
+    <caption><?php print $title; ?></caption>
+  <?php endif; ?>
+
+  <thead>
+    <tr>
+      <?php foreach ($header as $col => $col_header): ?>
+        <th class="views-matrix-col-header <?php print (!$col) ? 'views-matrix-col-first' : ''?>"><?php print $col_header; ?></th>
+      <?php endforeach; ?>
+    </tr>
+  </thead>
+
+  <tbody>
+    <?php foreach ($rows as $row_index => $row): ?>
+    <tr class="<?php print implode(' ', $row_classes[$row_index]); ?>">
+      <?php foreach ($row as $col_index => $content): ?>
+        <t<?php print ($col_index) ? 'd' : 'h class="views-matrix-row-header"'; ?>>
+          <?php print !empty($content) ? $content['data'] : ''; ?>
+        </t<?php print ($col_index) ? 'd' : 'h'; ?>>
+      <?php endforeach; ?>
+    </tr>
+    <?php endforeach; ?>
+  </tbody>
+</table>
